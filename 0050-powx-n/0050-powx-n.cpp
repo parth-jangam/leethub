60c4451d4b412e5e35e4de6a6bff9b50c9cc7ac5
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-11 15:35:10 +0530

    Time: 4 ms (42.81%), Space: 5.9 MB (95.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
 double myPow(double x, int n) {
-        return pow(x,n);
+        double ans=1.0;
+        long long m=abs(n);
+        while(m>0){
+            if(m%2==0){
+                x=x*x;
+                m=m/2;
+            }
+            else{
+                ans=ans*x;
+                m=m-1;
+            }
+        }
+        if(n<0){
+            ans=1/ans;
+        }
+    return ans;
     }
 };
