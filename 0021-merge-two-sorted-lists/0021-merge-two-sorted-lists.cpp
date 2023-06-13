f6505aa0bdd3a5372e04b81b5798898904df28fe
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-13 15:09:09 +0530

    Time: 12 ms (34.24%), Space: 14.7 MB (97.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,50 +10,49 @@
  */
 class Solution {
 public:
-   ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        if(list1==NULL && list2==NULL){
-            return NULL;
-        }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp=NULL;
+        ListNode* head=NULL;
         if(list1==NULL){
             return list2;
         }
         if(list2==NULL){
             return list1;
         }
-        ListNode* temp=NULL;
-        ListNode* head=NULL;
-        if(list1->val>list2->val){
+        if(list1!=NULL && list2!=NULL && list1->val>list2->val){
             temp=list2;
             list2=list2->next;
-            head=temp;
+            
         }
         else{
-            temp=list1;
-            list1=list1->next;
-            head=temp;
+            if(list1!=NULL && list2!=NULL){
+                temp=list1;
+                list1=list1->next;
+            }
         }
+        head=temp;
         while(list1!=NULL && list2!=NULL){
             if(list1->val>list2->val){
                 temp->next=list2;
-                list2=list2->next;
                 temp=temp->next;
+                list2=list2->next;
+            
             }
             else{
                 temp->next=list1;
-                list1=list1->next;
                 temp=temp->next;
-            }
+                list1=list1->next;
+            }   
         }
         while(list1!=NULL){
             temp->next=list1;
+            temp=temp->next;
             list1=list1->next;
-            temp=temp->next;
         }
-        while(list2!=NULL){
-             temp->next=list2;
-            list2=list2->next;
+         while(list2!=NULL){
+            temp->next=list2;
             temp=temp->next;
+            list2=list2->next;
         }
         return head;
     }
