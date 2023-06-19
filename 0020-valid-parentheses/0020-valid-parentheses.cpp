ea46a8e2586dc08ad168fdf16a94b1c2d03ba88b
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-19 15:22:31 +0530

    Time: 4 ms (40.29%), Space: 6.2 MB (79.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> p;
+        stack<char>st;
         for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            if((s[0]==']')||(s[0]=='}')||(s[0]==')')){
-                return false;
-            }
-            if((s[i] =='[') || (s[i] =='(' ) || (s[i] =='{')){
-                p.push(s[i]);
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
             }
             else{
-                if(!p.empty()){
-                    char top=p.top();
-                    if((s[i]==')' && top=='(')|| (s[i]=='}' && top=='{') ||(s[i]==']' && top=='[')){
-                        p.pop();
+                if(!st.empty()){
+                    if((s[i]=='}' && st.top()=='{') || (s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') ){
+                           
+                        
+                        st.pop();
                     }
                     else{
                         return false;
                     }
+                    
                 }
                 else{
                     return false;
-    
+                }
             }
+            
         }
-        }
-                                                
-        if(p.empty())
-            return true;
-        
-        else
+        if(!st.empty()){
             return false;
+        }
+        return true;
     }
 };
