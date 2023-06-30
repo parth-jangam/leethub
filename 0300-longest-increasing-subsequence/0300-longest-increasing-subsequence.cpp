23be60b1654ff1ec460a5ce70efe571825879e4e
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-06-30 10:11:44 +0530

    Time: 1112 ms (23.98%), Space: 292.5 MB (7.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int maxi_sub(vector<int>&nums,int i,int prev,vector<vector<int>>&dp){
+    int max_len(vector<int>& nums,int i,int prev,vector<vector<int>>&dp){
         if(i>=nums.size()){
             return 0;
         }
-        if(dp[i][prev+1]!=-1){
-            return dp[i][prev+1];
+        if(dp[prev+1][i]!=-1){
+            return dp[prev+1][i];
         }
-        int a=0;
-        int b=0;
+        int take=0;
+        int not_take=0;
         if(prev==-1 || nums[prev]<nums[i]){
-            a=max(1+maxi_sub(nums,i+1,i,dp),maxi_sub(nums,i+1,prev,dp));
+            take=max(1+max_len(nums,i+1,i,dp),max_len(nums,i+1,prev,dp));
         }
         else{
-            b=maxi_sub(nums,i+1,prev,dp);
+            not_take=max_len(nums,i+1,prev,dp);
         }
-        return dp[i][prev+1]=max(a,b);
+        return dp[prev+1][i]= max(take,not_take);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return maxi_sub(nums,0,-1,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+1,-1));
+        return max_len(nums,0,-1,dp);
     }
 };
