e200c8fb0f67db7e9dbbd099a1375d5508d82e37
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-04 13:33:06 +0530

    Time: 126 ms (31.93%), Space: 47 MB (32.47%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,24 +3,24 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mappe;
         int sum=0;
-        mappe[0]=1;
+        // mappe[0]=1;
         int i=0;
         int count=0;
         while(i<nums.size()){
             sum+=nums[i];
-            int dig=sum-k;
-            count+=mappe[dig];
+            if(sum==k){
+                count++;
+                count+=mappe[0];
+            }
+            else{
+                int check1=sum-k;
+                // int check2=sum+k;
+                if(mappe[check1]!=0){
+                    count+=mappe[check1];
+                }
+                
+            }
             mappe[sum]++;
-            //     int check1=sum-k;
-            //     int check2=sum+k;
-            //     if(mappe[check1]!=0){
-            //         count+=mappe[check1];
-            //     }
-            //     if(check1!=check2 && mappe[check2]!=0){
-            //         count+=mappe[check2];
-            //     }
-            // }
-            // mappe[sum]++;
             i++;
         }
         return count;
