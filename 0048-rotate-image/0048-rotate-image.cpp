42fb63a8d0ef8a8dbc154f362329adb3e214a751
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-24 19:57:59 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (84.96%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>>temp=matrix;
-        int row=matrix.size();
-        int column=matrix[0].size();
-        int l=0;
-        for(int i=column-1;i>=0;i--){
-            int k=0;
-            for(int j=0;j<row;j++){
-                matrix[j][i]=temp[l][k];
-                k++;
-            }
-            l++;
-        }
+    void rotate(vector<vector<int>>& mat) {
+        // vector<vector<int>>temp=matrix;
+        // int row=matrix.size();
+        // int column=matrix[0].size();
+        // int l=0;
+        // for(int i=column-1;i>=0;i--){
+        //     int k=0;
+        //     for(int j=0;j<row;j++){
+        //         matrix[j][i]=temp[l][k];
+        //         k++;
+        //     }
+        //     l++;
+        // }
+        for(int i=0;i<mat.size();i++){
+		for(int j=0;j<i;j++){
+			swap(mat[i][j],mat[j][i]);
+		}
+	}
+	for(int i=0;i<mat.size();i++){
+		reverse(mat[i].begin(),mat[i].end());
+	}
     }
 };
