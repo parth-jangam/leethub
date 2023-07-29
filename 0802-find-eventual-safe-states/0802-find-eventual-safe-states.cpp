f59f944a64226200c62a8a848dac328e90982445
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-29 16:59:01 +0530

    Time: 156 ms (94.70%), Space: 58.9 MB (48.27%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
-    bool iscyclicdfs(int i,unordered_map<int,bool>&vis,unordered_map<int,bool>&dfsvis,  vector<int>adj[],vector<int>&node){
-       vis[i]=true;
-       dfsvis[i]=true;
-       
-       for( auto neighbour:adj[i]){
-           if(!vis[neighbour]){
-                bool checkcycle=iscyclicdfs(neighbour,vis,dfsvis,adj,node);
-               if(checkcycle)
-               return true;
-           }
-           
-           else if(dfsvis[neighbour]){
-              return true;
-           }
-    
-       }
-        node[i]=1;
-       dfsvis[i]=false;
-       return false;
-   }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        
-        
-        vector<int>adj[graph.size()];
-        for(int i=0;i<graph.size();i++){
-            // bool flag=0;
-            for(int j=0;j<graph[i].size();j++){
-                adj[i].push_back(graph[i][j]);
+        int n=graph.size();
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+            for(auto x:graph[i]){
+                adj[x].push_back(i);
             }
-            
         }
-        vector<int>ans;
-        unordered_map<int,bool>vis;
-        unordered_map<int,bool>dfsvis;
-        vector<int>node(graph.size(),-1);
-        for(int i=0;i<graph.size();i++){
-           // if(!vis[i]){
-               bool checkcycle=iscyclicdfs(i,vis,dfsvis,adj,node);
-               if(!checkcycle || node[i]==1){ans.push_back(i);}
-               // return true;
-           // }
-       }
-  // return false;
-        return ans;
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto x:adj[i]){
+                indegree[x]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+            for(auto x:adj[node]){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+        sort(topo.begin(),topo.end());
+        return topo;
     }
 };
