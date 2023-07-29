18ba197e4e89d41b0c518cd27ee7cf58f67afc18
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-29 16:11:55 +0530

    Time: 16 ms (96.85%), Space: 13.7 MB (67.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
-        bool iscyclicdfs(int i,unordered_map<int,bool>&vis,unordered_map<int,bool>&dfsvis,  vector<int>adj[]){
-       vis[i]=true;
-       dfsvis[i]=true;
-       
-       for( auto neighbour:adj[i]){
-           if(!vis[neighbour]){
-                bool checkcycle=iscyclicdfs(neighbour,vis,dfsvis,adj);
-               if(checkcycle)
-               return true;
-           }
-           
-           else if(dfsvis[neighbour]){
-              return true;
-           }
-    
-       }
-       dfsvis[i]=false;
-       return false;
-   }
-   bool isCyclic(int V, vector<int> adj[]) {
-        unordered_map<int,bool>vis;
-        unordered_map<int,bool>dfsvis;
-        for(int i=0;i<V;i++){
-           if(!vis[i]){
-               bool checkcycle=iscyclicdfs(i,vis,dfsvis,adj);
-               if(checkcycle)
-               return true;
-           }
+    bool is_cycle(vector<int>adj[],int i,vector<int>&vis,vector<int>&pathvis){
+        vis[i]=1;
+        pathvis[i]=1;
+        for(auto x:adj[i]){
+            if(!vis[x]){
+                if(is_cycle(adj,x,vis,pathvis)==true){
+                    return true;
+                }
+            }
+            else{
+                if(pathvis[x]){
+                    return true;
+                }
+            }
         }
-        return false; 
-   }
-
+        pathvis[i]=0;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,int>mp;
-        int i=0;
+        
         vector<int>adj[numCourses];
-        while(i<prerequisites.size()){
-            // mp[prerequisites[i][0]]=prerequisites[i][1];
+        for(int i=0;i<prerequisites.size();i++){
+        
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            i++;
         }
-        if(isCyclic(numCourses,adj)){
-            return false;
+        vector<int>vis(numCourses,0);
+        // int vis[numCourses]={0};
+        vector<int>pathvis(numCourses,0);
+        // int pathvis[numCourses]={0};
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(is_cycle(adj,i,vis,pathvis)){
+                    return false;
+                }
+            }
         }
         return true;
-        
-        
     }
 };
