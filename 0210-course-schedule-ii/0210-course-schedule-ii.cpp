60303ee225d16fd52a2aaeda2044de5066d6ce12
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-29 16:38:08 +0530

    Time: 16 ms (96.14%), Space: 13.9 MB (47.33%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     
-    bool dfs(vector<int>adj[],int i,vector<int>&ans,vector<int>&vis,vector<int>&pathvis){
+//     bool dfs(vector<int>adj[],int i,vector<int>&ans,vector<int>&vis,vector<int>&pathvis){
+//         vis[i]=1;
+//         pathvis[i]=1;
+//         for(auto x:adj[i]){
+//             if(vis[x]==0){
+//                 if(dfs(adj,x,ans,vis,pathvis)==true){
+                   
+//                     return true;
+//                 }
+//             }
+//             else if(pathvis[x]==1){
+//                  // cout<<"hello";
+//                 return true;
+//             }
+//         }
+//         pathvis[i]=0;
+//         ans.push_back(i);
+//         return false;
+//     }
+     bool is_cycle(vector<int>adj[],int i,vector<int>&vis,vector<int>&pathvis,vector<int>&topo){
         vis[i]=1;
         pathvis[i]=1;
         for(auto x:adj[i]){
-            if(vis[x]==0){
-                if(dfs(adj,x,ans,vis,pathvis)==true){
-                   
+            if(!vis[x]){
+                if(is_cycle(adj,x,vis,pathvis,topo)==true){
                     return true;
                 }
             }
-            else if(pathvis[x]==1){
-                 // cout<<"hello";
-                return true;
+            else{
+                if(pathvis[x]){
+                    return true;
+                }
             }
         }
+        topo.push_back(i);
         pathvis[i]=0;
-        ans.push_back(i);
         return false;
     }
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[numCourses];
         
@@ -29,21 +49,20 @@ public:
             
         }
         
+        
         vector<int>vis(numCourses,0);
         // int vis[numCourses]={0};
         vector<int>pathvis(numCourses,0);
-        // int pathvis[numCourses]={0}
-        vector<int>ans;
-        // vector<int>fina;
+        // int pathvis[numCourses]={0};
+        vector<int>topo;
         for(int i=0;i<numCourses;i++){
-            if(vis[i]==0){
-                if(dfs(adj,i,ans,vis,pathvis)==true){
-                    ans.clear();
-                    
-                    return ans;
+            if(!vis[i]){
+                if(is_cycle(adj,i,vis,pathvis,topo)){
+                    return {};
                 }
             }
         }
-        return ans;
+        // cout<<topo.size()<<endl;
+        return topo;
     }
 };
