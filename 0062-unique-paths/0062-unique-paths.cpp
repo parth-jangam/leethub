0465033f392f47662ba5e3d40f5303144e22f141
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-29 19:11:36 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (64.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int paths(int i,int j,int m, int n,vector<vector<int>>&dp){
+    int paths(int m,int n,int i,int j,vector<vector<int>>&dp){
         if(i==m-1 && j==n-1){
             return 1;
         }
-        if(dp[i][j]!=-1){
+        if(i==m || i<0){
+            return 0;
+        }
+        if(j==n || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=0){
             return dp[i][j];
         }
-        int path=0;
-        if(i>=0 && i<m && j>=0 && j<n){
-            path+=paths(i+1,j,m,n,dp);
-            path+=paths(i,j+1,m,n,dp);
-        }
-        return dp[i][j]=path;
-        
+        int right=0,left=0;
+        right+=paths(m,n,i+1,j,dp);
+        left+= paths(m,n,i,j+1,dp);
+        return dp[i][j] =right+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return paths(0,0,m,n,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        return paths(m,n,0,0,dp);
     }
 };
