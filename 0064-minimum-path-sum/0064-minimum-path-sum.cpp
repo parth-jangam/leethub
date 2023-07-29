9eb04700132161f401419f9ff63902054596f073
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-29 19:38:41 +0530

    Time: 7 ms (93.26%), Space: 10 MB (71.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int mini(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<int>>&dp){
+    int mini(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>&dp){
         if(i==m-1 && j==n-1){
             return grid[i][j];
         }
         if(i>=m || j>=n){
-            return INT_MAX;
+            return 1e9;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int bottom=INT_MAX;
-        int right=INT_MAX;
-        if(i>=0 && j>=0 && i<m-1 && j<n){
-            bottom=grid[i][j]+mini(grid,i+1,j,m,n,dp);
-        }
-        if(i>=0 && j>=0 &&i<m && j<n-1){
-            right=grid[i][j]+mini(grid,i,j+1,m,n,dp);
-        }
-        return dp[i][j]= min(bottom,right);
+        int right=0;
+        int left=0;
+        right=grid[i][j]+mini(i+1,j,m,n,grid,dp);
+        left=grid[i][j]+mini(i,j+1,m,n,grid,dp);
+        return dp[i][j]= min(right,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return mini(grid,0,0,m,n,dp);
+        return mini(0,0,m,n,grid,dp);
     }
 };
