f9f780945c0e2a3ba7aeb7fbe9c889989bdfbc1b
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-30 22:56:55 +0530

    Time: 54 ms (97.99%), Space: 95.8 MB (17.60%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    bool is_pos(vector<int>& nums,int sum,int i,vector<vector<int>>&dp){
-        if(sum==0){
+    bool is_possible(int target,int i,vector<int>& nums,vector<vector<int>>&dp){
+        if(target==0){
             return true;
         }
-        if(i>=nums.size())return false;
-        if(sum<0)return false;
-        if(dp[i][sum]!=-1){
-            return dp[i][sum];
-        }
-        bool take=false;
-        if(sum>=nums[i]){
-            take=is_pos(nums,sum-nums[i],i+1,dp);
-        }
-        bool not_take=is_pos(nums,sum,i+1,dp);
-        return dp[i][sum]= take || not_take;
-    }
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int i=0;
-        while(i<nums.size()){
-            sum+=nums[i];
-            i++;
-        }
-        if(sum%2!=0){
+        if(i>=nums.size() || target<0){
             return false;
         }
-        sum/=2;
+        if(dp[i][target]!=-1){
+            return dp[i][target];
+        }
+        return dp[i][target]= is_possible(target-nums[i],i+1,nums,dp) || is_possible(target,i+1,nums,dp);
+    }
+    bool canPartition(vector<int>& nums) {
+        int i=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0)return false;
+        int target=sum/2;
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return is_pos(nums,sum,0,dp);
-        
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        return is_possible(target,0,nums,dp);
     }
 };
