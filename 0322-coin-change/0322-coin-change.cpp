cd06e7f73ba64f40e76e7850031f2df1a96f6aad
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-07-31 15:05:03 +0530

    Time: 140 ms (38.35%), Space: 50 MB (8.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int anss(int i,vector<int>&coins,int amount,vector<vector<int>>&dp){
-        if(i==0){
-            if(amount%coins[i]==0){
-                return amount/coins[i];
-            }
+    int mini(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
+        if(amount==0){
+            return 0;
+        }
+        if(i==coins.size() || amount<0){
             return 1e9;
         }
         if(dp[i][amount]!=-1){
             return dp[i][amount];
         }
         int pick=1e9;
+        int not_pick=1e9;
         if(amount>=coins[i]){
-            pick=1+anss(i,coins,amount-coins[i],dp);
+            pick=1+min(mini(coins,amount-coins[i],i,dp),mini(coins,amount-coins[i],i+1,dp));
         }
-        int not_pick=anss(i-1,coins,amount,dp);
-        return dp[i][amount]=min(pick,not_pick);
+        not_pick=mini(coins,amount,i+1,dp);
+        return dp[i][amount]= min(pick,not_pick);
     }
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
-        int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int a=anss(n-1,coins,amount,dp);
-        if(a==1e9){
-            return -1;
-        }
-    return a;
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int ans= mini(coins,amount,0,dp);
+        
+        if(ans==1e9)return -1;
+        return ans;
     }
 };
