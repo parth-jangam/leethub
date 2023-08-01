ddba56321c387b8b690eed082acc628a85400d18
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-08-01 18:47:30 +0530

    Time: 64 ms (64.22%), Space: 24.4 MB (66.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-    int lcs(string text1, string text2,int i,int j,vector<vector<int>>&dp){
+    int longest(string text1,string text2,int i,int j,vector<vector<int>>&dp){
         if(i>=text1.length() || j>=text2.length()){
             return 0;
         }
         if(dp[i][j]!=0){
-            dp[i][j];
+            return dp[i][j];
         }
         int take=0;
-        int not_take=0;
         if(text1[i]==text2[j]){
-            take=1+lcs(text1,text2,i+1,j+1,dp);
+            take=1+longest(text1,text2,i+1,j+1,dp);
         }
-        else{
-            not_take=max(lcs(text1,text2,i+1,j,dp),lcs(text1,text2,i,j+1,dp));
-        }
+        int not_take=max(longest(text1,text2,i+1,j,dp),longest(text1,text2,i,j+1,dp));
         return dp[i][j]= max(take,not_take);
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m=text2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        
-        for(int i=0;i<=n;i++){
-            dp[i][m]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[n][i]=0;
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
+        int m=text1.length();
+        int n=text2.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
                 int take=0;
-                int not_take=0;
                 if(text1[i]==text2[j]){
                     take=1+dp[i+1][j+1];
                 }
-                else{
-                    not_take=max(dp[i+1][j],dp[i][j+1]);
-                }
+                int not_take=max(dp[i+1][j],dp[i][j+1]);
                 dp[i][j]= max(take,not_take);
 
             }
