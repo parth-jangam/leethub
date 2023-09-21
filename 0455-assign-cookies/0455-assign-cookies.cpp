7e191b33d507398079210d8b731804688306f759
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2023-09-21 16:52:47 +0530

    Time: 20 ms (73.42%), Space: 18 MB (6.58%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int count=0;
+        int i=g.size()-1;
+        int j=s.size()-1;
+        while(i>=0 && j>=0){
+            if(s[j]>=g[i]){
+                count++;
+                j--;
+                i--;
+            }
+            else{
+                i--;
+            }
+        }
+        return count;
+    }
+};
