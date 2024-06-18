5677a9581398332c544c5924d42abc454ec0317f
parth-jangam <117796836+parth-jangam@users.noreply.github.com>
2024-06-18 12:31:10 +0530

    Time: 7 ms (27.83%), Space: 10.5 MB (42.54%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
      int removeElement(vector<int>& nums, int val) {
-//         int p=nums.size();
-//         int i=0;
-//         int l=p;
+        int p=nums.size();
+        int i=0;
+      
         
         
 
-//         while(p>i){
-//             if(nums[i]==val){
-//                 nums[i] = nums[p - 1];
-//                 // nums.erase(nums.begin()+i);
-//                 l--;
+        while(p>i){
+            if(nums[i]==val){
+                // nums[i] = nums[p - 1];
+                nums.erase(nums.begin()+i);
+                p--;
                 
-//             }
-//             i++;
+            }
+            else{
+            i++;
+            }
             
-//         }
+        }
         
-//         return l;
-        int length = nums.size(), index = 0;
-		while ( index < length )
-		{
-			 if ( nums[index] == val )
-			 {
-				 nums[index] = nums[length - 1];
-				 -- length;
-			 }
+        return p;
+//         int length = nums.size(), index = 0;
+// 		while ( index < length )
+// 		{
+// 			 if ( nums[index] == val )
+// 			 {
+// 				 nums[index] = nums[length - 1];
+// 				 -- length;
+// 			 }
 				
-			else ++index;
-		}
-		 return length;
+// 			else ++index;
+// 		}
+// 		 return length;
     }
 };
